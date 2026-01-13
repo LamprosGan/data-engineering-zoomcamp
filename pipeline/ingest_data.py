abf3aa3c60c4db18299b1703d66cf2e3f41a2797
Lampros Ganias <lampgns96@gmail.com>
2026-01-13 18:01:41 +0000

    pipeline code working

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -6,7 +6,7 @@ from sys import prefix
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
-
+import click
 
 
 # Read a sample of the data
@@ -36,27 +36,26 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
-def run():
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host = 'localhost'
-    pg_port = '5432'
-    pg_db = 'ny_taxi'
-
-    year =2021
-    month = 1
-
-    target_table = 'yellow_taxi_data'
+@click.command()
+@click.option('--pg-user', default='root', show_default=True, help='Postgres user')
+@click.option('--pg-pass', default='root', show_default=True, help='Postgres password', hide_input=True)
+@click.option('--pg-host', default='localhost', show_default=True, help='Postgres host')
+@click.option('--pg-port', default=5432, show_default=True, type=int, help='Postgres port')
+@click.option('--pg-db', default='ny_taxi', show_default=True, help='Postgres database')
+@click.option('--year', default=2021, show_default=True, type=int, help='Data year')
+@click.option('--month', default=1, show_default=True, type=int, help='Data month (1-12)')
+@click.option('--target-table', default='yellow_taxi_data', show_default=True, help='Target table name')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table):
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
     url = f'{prefix}yellow_tripdata_{year}-{month:02d}.csv.gz'
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
-    
+
     df_iter = pd.read_csv(
         url,
-        dtype=dtype, 
-        parse_dates=parse_dates, 
-        iterator=True, 
-        chunksize=100000
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=100000,
     )
     
     
